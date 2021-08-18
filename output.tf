d35a6610f802dc666c1c701ff045b2f403c9b6fa
Christian Nuss <christian.nuss@gmail.com>
2021-08-18 15:49:02 -0700

    Add outputs

--- output.tf
+++ output.tf
@@ -1,1 +1,11 @@
+output "region" {
+  value = var.region
+}
 
+output "role" {
+  value = "arn:aws:iam::${var.account_id}:role/${var.root_email}"
+}
+
+output "api_gateway_stages" {
+  value = module.aws_api_gateway.api_gateway_stages
+}
