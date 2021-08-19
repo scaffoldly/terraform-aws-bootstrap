cd51e2cfc97d8ae3ff1c0810ebba76836dd16f54
Christian Nuss <christian.nuss@gmail.com>
2021-08-19 15:17:34 -0700

    Format output

--- output.tf
+++ output.tf
@@ -1,11 +1,14 @@
 output "region" {
-  value = var.region
+  value       = var.region
+  description = "Re-output of var.region"
 }
 
 output "role" {
-  value = "arn:aws:iam::${var.account_id}:role/${var.root_email}"
+  value       = "arn:aws:iam::${var.account_id}:role/${var.root_email}"
+  description = "IAM role for the managed account"
 }
 
 output "api_gateway_stages" {
-  value = module.aws_api_gateway.api_gateway_stages
+  value       = module.aws_api_gateway.api_gateway_stages
+  description = "Output of api_gatway_stages from scaffoldly/terraform-aws-api-gateway"
 }
