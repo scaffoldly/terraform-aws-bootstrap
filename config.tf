1babe1c0761c9dfb1afcf67ee29dd9980a332f69
Christian Nuss <christian.nuss@gmail.com>
2021-08-27 08:08:38 -0700

    Remove aws version constraint

--- config.tf
+++ config.tf
@@ -5,7 +5,6 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "3.55.0"
       configuration_aliases = [aws.root]
     }
   }
