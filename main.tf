ae1dfbef4c758aecd50f760dfd73903c8870f28b
Christian Nuss <cnuss@users.noreply.github.com>
2021-10-12 19:20:50 -0700

    scaffoldly/api-gateway/aws 1.0.8

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "kms" {
 
 module "aws_api_gateway" {
   source  = "scaffoldly/api-gateway/aws"
-  version = "1.0.7"
+  version = "1.0.8"
 
   subdomain = var.serverless_api_subdomain
   stages    = var.stages
